b9a5887d9131ebac57202761ad6f213ef44840b1
Creator125 <85368990+Creator125@users.noreply.github.com>
2023-08-09 16:06:40 -0500

    Nuevo proyecto

---
+++ Semestre_3/Concesionario/app/src/main/java/com/example/concesionario/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.concesionario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Clientes(View view){ //Metodo de la actividad de Clientes
+        Intent intClientes = new Intent(this, ClientesActivity.class);
+        startActivity(intClientes);
+    }
+
+    public void Vehiculos(View view){ //Metodo de la actividad de Vehiculos
+        Intent intVehiculos = new Intent(this, VehiculosActivity.class);
+        startActivity(intVehiculos);
+    }
+
+    public void Facturas(View view){ //Metodo de la actividad de Facturas
+        Intent intFacturas = new Intent(this, FacturasActivity.class);
+        startActivity(intFacturas);
+    }
+}
