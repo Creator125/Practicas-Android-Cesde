6eabafcb5931e09b1b673debdd9d073c2a4d4a2d
Creator125 <85368990+Creator125@users.noreply.github.com>
2023-08-11 16:52:51 -0500

    Concesionario/: Arregrando problemas de compartibilidad para la ejecucion de la aplicación

---
+++ Semestre_3/Concesionario/app/src/main/java/com/example/concesionario/ClsOpenHelper.java
@@ -0,0 +1,40 @@
+package com.example.concesionario;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class ClsOpenHelper extends SQLiteOpenHelper {
+    public ClsOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //Tabla de clientes
+        db.execSQL("CREATE TABLE TblClietes(IdCliente TEXT PRIMARY KEY, " +
+                "NomCliente TEXT NOT NULL, TelCliente TEXT NOT NULL, Activo TEXT DEFAULT 'Si')");
+        //Tabla de Vehiculos
+        db.execSQL("CREATE TABLE TblVehiculos(Placa TEXT PRIMARY KEY," +
+                " Modelo TEXT NOT NULL, Marca TEXT NOT NULL, Valor INTEGER NOT NULL, Activo TEXT DEFAULT 'Si')");
+        //Tabla de Facturas
+        db.execSQL("CREATE TABLE TblFacturas(CodFacturas TEXT PRIMARY KEY," +
+                " Fecha TEXT NOT NULL, IdtCliente TEXT NOT NULL, Activo TEXT DEFAULT 'Si'," +
+                " CONSTRAINT pk_factura FOREIGN KEY (IdCliente) REFERENCES TblClietes(IdCliente))");
+        //Tabla de Detalle_Factura
+        db.execSQL("CREATE TABLE TblDetalle_Factura(CodFactura TEXT NOT NULL," +
+                " ValorRenta INTEGER NOT NULL, CONSTRAINT pk_detalle PRIMARY KEY (CodFactura, Placa)," +
+                " FOREIGN KEY (CodFactura) REFERENCES TblFacturas(CodFacturas), " +
+                "FOREIGN KEY (Placa) REFERENCES TblVehiculos(Placa))");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE TblDetalle_Factura");
+        db.execSQL("DROP TABLE TblFacturas");
+        db.execSQL("");
+    }
+
+    }
--- Semestre_3/Concesionario/app/src/main/java/com/example/concesionario/MainActivity.java
+++ Semestre_3/Concesionario/app/src/main/java/com/example/concesionario/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
